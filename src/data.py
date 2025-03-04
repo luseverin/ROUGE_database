d6b68aea658567285a57f0051c4c9bea5cc016b7
luseverin <luca.severino@usys.ethz.ch>
2025-03-04 15:57:10 +0100

    Update paths

--- src/data.py
+++ src/data.py
@@ -1,5 +1,6 @@
 ##paths and variables
 DATA_PATH = '../Data_backup/'
-DATA_IN_PATH = '../Data_backup/report_jsons/'
-DATA_OUT_PATH = '../Data_backup/results_llm/'
+DATA_IN_JSONS = DATA_PATH + 'report_jsons/'
+DATA_OUT_LLMS = DATA_PATH + 'results_llm/'
+DATA_LABELLED = DATA_PATH + 'labelled/'
 
