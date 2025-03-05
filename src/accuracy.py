5a3c2cef967c47326faec9c79e39eb328c58a209
laurahasbini <laura.hasbini@ensta-paris.fr>
2025-03-05 14:23:02 +0100

    Accuracy computation per reports and for hazardType/hazardSubtype

--- src/accuracy.py
+++ src/accuracy.py
@@ -7,6 +7,7 @@ import pycountry
 from sklearn.metrics.pairwise import cosine_similarity
 import copy as cp
 from constants import *
+import ast
 
 
 unique_countries_ISO = [country.alpha_3 for country in pycountry.countries]
@@ -15,7 +16,7 @@ pattern = '|'.join(map(re.escape, unique_country_names))
 unique_dict = {
     'hazardType' : maintype_to_subytpe_emdat.keys(),
     'hazardSubtypes' : ast.literal_eval(hazard_all_subtype_emdat),
-    'country' : unique_countries_ISO
+    'country' : unique_country_names
 }
 
 # Functions for accuracy computation
@@ -182,7 +183,6 @@ def calculate_precision_per_report(df_chat, df_labelled, precision_columns_list,
         for precision_column in precision_columns_list:
             if precision_column in grouping_columns : 
                 index = grouping_columns.index(precision_column)
-                unique_list = unique_dict[precision_column]
     
                 #Select the unique set
                 tmp_labelled_event_unique = [haz[index] for haz in tmp_labelled_event.groups.keys()]
@@ -190,10 +190,15 @@ def calculate_precision_per_report(df_chat, df_labelled, precision_columns_list,
                 
                 #For Hazard and Country, accuracy is computed by checking is the found attributes are matching
                 if precision_column in ["hazardType", "hazardSubtypes", "country"] :
-    
+                    unique_list = unique_dict[precision_column]
+                    
                     # Create binary vectors for the two lists
-                    vector1 = [1 if hazard in sorted(tmp_unique) else 0 for hazard in unique_list]
-                    vector2 = [1 if hazard in sorted(tmp2_unique) else 0 for hazard in unique_list]
+                    vector1 = [1 if hazard in sorted(tmp_labelled_event_unique) else 0 for hazard in unique_list]
+                    vector2 = [1 if hazard in sorted(tmp_chat_event_unique) else 0 for hazard in unique_list]
+
+                    # if precision_column == "country" : 
+                    #     print(vector1)
+                    #     print(vector2)
     
                     # Convert the vectors to numpy arrays and reshape them
                     vector1 = np.array(vector1).reshape(1, -1)
@@ -208,8 +213,8 @@ def calculate_precision_per_report(df_chat, df_labelled, precision_columns_list,
                 #Do not look at the exact value
                 elif precision_column in ["startYear", "startMonth", "startDay", "endYear", "endMonth", "endDay"] :
                     # Create binary vectors for the two lists
-                    n_tmp = sum(1 for value in tmp_labelled_event_unique[precision_column] if (value != 'NULL') and (value != "missing"))
-                    n_tmp2 = sum(1 for value in tmp_chat_event_unique[precision_column] if (value != 'NULL') and (value != "missing"))
+                    n_tmp = sum(1 for value in tmp_labelled[precision_column].unique() if (value != 'NULL') and (value != "missing"))
+                    n_tmp2 = sum(1 for value in tmp_chat[precision_column].unique() if (value != 'NULL') and (value != "missing"))
     
                     #cos_sim = cosine_similarity(vector1, vector2)[0][0]
                     if n_tmp != 0 :
@@ -226,12 +231,11 @@ def calculate_precision_per_report(df_chat, df_labelled, precision_columns_list,
                 # for i in keys:
                 #     labelled_event_loop = reports_labelled_event.get_group(i) 
                         
-                n_tmp = sum(1 for value in tmp_labelled[precision_column] if (value != 'NULL') and (value != "missing"))
-                n_tmp2 = sum(1 for value in tmp_chat[precision_column] if (value != 'NULL') and (value != "missing"))
+                n_tmp = sum(1 for value in tmp_labelled[precision_column].unique() if (value != 'NULL') and (value != "missing"))
+                n_tmp2 = sum(1 for value in tmp_chat[precision_column].unique() if (value != 'NULL') and (value != "missing"))
                 if n_tmp != 0 :
                     results[precision_column]["psum"] += n_tmp2/n_tmp
                     results[precision_column]["count"] += 1
-                
     # Calculate precision and create a DataFrame
     precision_values = []
     for precision_column in precision_columns_list:
@@ -244,4 +248,20 @@ def calculate_precision_per_report(df_chat, df_labelled, precision_columns_list,
     precision_df = pd.DataFrame([precision_values], columns=precision_columns_list)
     return precision_df
 
+def calculate_precision_per_hazardType(df_chat, df_labelled, precision_columns_list, unique_dict=unique_dict):
+    precision_dict = {}
+    for hazard in unique_dict["hazardType"] :
+        df_chat_haz = df_chat.loc[df_chat.hazardType == hazard].copy()
+        df_labelled_haz = df_labelled.loc[df_labelled.hazardType == hazard].copy()
+        precision_haz = calculate_precision_per_report(df_chat_haz, df_labelled_haz, precision_columns_list, unique_dict)
+        precision_dict[hazard] = precision_haz
+    return precision_dict
 
+def calculate_precision_per_hazardSubtypes(df_chat, df_labelled, precision_columns_list, unique_dict=unique_dict):
+    precision_dict = {}
+    for hazardSubtype in unique_dict["hazardSubtypes"] : 
+        df_chat_haz = df_chat.loc[df_chat.hazardSubtypes == hazardSubtype].copy()
+        df_labelled_haz = df_labelled.loc[df_labelled.hazardSubtypes == hazardSubtype].copy()
+        precision_haz = calculate_precision_per_report(df_chat_haz, df_labelled_haz, precision_columns_list, unique_dict)
+        precision_dict[hazardSubtype] = precision_haz
+    return precision_dict
