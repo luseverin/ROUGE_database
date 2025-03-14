13d26f0e7cb84c235923cff0a9a5153f8fbdeee8
laurahasbini <laura.hasbini@ensta-paris.fr>
2025-03-14 10:56:45 +0100

    Select path according to user
    @Luca : Double Check your username

--- src/data.py
+++ src/data.py
@@ -1,6 +1,17 @@
 ##paths and variables
-DATA_PATH = '../Data_backup/'
+import getpass
+DATA_PATH_LS = '../Data_backup/'
+DATA_PATH_LH = "/scratchx/lhasbini/como_school/"
+
+user = getpass.getuser()
+if user == "lhasbini" : 
+    DATA_PATH = DATA_PATH_LH
+elif user == "lseverino" : ##### TO CHANGE FOR LUCA
+    DATA_PATH = DATA_PATH_LS
+else:
+    raise ValueError(f"Unknown user: {user}")
+    
 DATA_IN_JSONS = DATA_PATH + 'report_jsons/'
 DATA_OUT_LLMS = DATA_PATH + 'results_llm/'
 DATA_LABELLED = DATA_PATH + 'labelled/'
-
+DATA_FIGURE = DATA_PATH + 'figure/'
