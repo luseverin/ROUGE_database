c99a2f05bb783af36b7624c864cda20f8b7115e1
luseverin <luca.severino@usys.ethz.ch>
2025-03-25 16:39:03 +0100

    Move dict for hazard main and sub types kwsearch to constants

--- src/constants.py
+++ src/constants.py
@@ -27,6 +27,17 @@ maintype_to_subytpe_emdat = {'Drought': ['drought'],
   'extra-tropical storm',
   'tropical storm']}
 
+hazard_subtype_kw_searc ={
+                        'Drought': r"drought.*|dry spell.*",
+                        'Wildfire': r"fire.*|forestfire.*|wildfire.*|landfire.*|bushfire.*|forest fire.*|wild fire.*|land fire.*|bush fire.*" ,
+                        'Earthquake' : r"ground movement.|tsunami.",
+                        'Mass movement': r"avalanche.|land slide.*|landslide.*|rockfall.*|sudden subsidence.|mudslide.|mass movement.*",
+                        'Volcanic activity' : r"ash fall.|lava flow.|pyroclastic flow.|lahar",
+                        'Flood': r"flood.*|inundation.*|coastal flood.|flash flood.|riverine flood.|ice jam flood.",
+                        'Wave action' : r"rogue wave.|seiche",
+                        'Extreme temperature' : r"cold wave.*|coldwave.*|cold spell.*|heat wave.*|heatwave.*|heat episode.*|((heat|hot) spell).*|heat stress.*|severe winter conditions.",
+                        'Storm' : r"derecho.|hail.|lightning.|winterstorm.|storm surge.|tornado.|winter storm.|extra-tropical storm.|tropical storm.|typhoon.|hurricane.|storm.*|superstorm.*|windstorm.*|snowstorm.*|blizzard.*|thunderstorm.*" }
+
 #impact types dict
 impact_types_dict = {
     "infrastructures": ["roads", "healthcare", "hospitals", "schools", "powerplants", "bridges"],
