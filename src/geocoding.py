0600e8815dc138d20a1ad4b969d062a06e000c2a
laurahasbini <laura.hasbini@ensta-paris.fr>
2025-04-18 09:22:16 +0200

    Geocoding of the impact

--- src/geocoding.py
+++ src/geocoding.py
@@ -62,6 +62,8 @@ def geocoding_reports_location(df, print_info=False, locations_levels=['country'
         if finest_loc_id:
             time_last_request = time.time()
             if finest_loc_id != "country" : 
+                print(finest_loc_vals)
+                print(type(finest_loc_vals))
                 nominatim_query=finest_loc_vals+", "+country
             else : 
                 nominatim_query=country
@@ -213,4 +215,89 @@ def geocoding_reports_region_cities(df, print_info=False, locations_levels=['cou
                 else :
                     if print_info :
                         print("WRONG location "+nominatim_result.raw['name']+" for "+finest_loc_vals+", similarity = "+str(similarity))
+    return df_geo
+
+
+# Geocoding of impact 
+# For impact we cannot separate the locations to have only one location per row --> Need to keep as a list 
+# The list of locations will be further divided into a list of cities/state/regions as well as a list of lat/lon for each impact
+
+def geocode_single_location(location, locations_levels=['country', 'region', 'state', 'city'], time_last_request=time.time(), print_info=False, similarity_th=0.4) :
+    # Geocode the given location
+    time_new_request = time.time()
+    if time_new_request - time_last_request <= 1:
+        time.sleep(1)#time_new_request - time_last_request)
+    nominatim_result = geolocator.geocode(location, exactly_one=True, language="en")
+    time_last_request = time_new_request
+
+    if nominatim_result is None :
+        if print_info : 
+            print("No results for query: ", location)
+        return None, time_last_request
+    else:
+        ## Double check that the known information is similar to the one found
+        finest_loc_vals_cleaned = remove_admin_words(location)
+        nominatim_result_cleaned = remove_admin_words(nominatim_result.raw['name'])
+        similarity = rotated_levenshtein_similarity(finest_loc_vals_cleaned, nominatim_result_cleaned)
+
+        if similarity > similarity_th :
+            location_dict = {}
+            if print_info : 
+                print("CORRECT location "+nominatim_result.raw['name']+" for "+finest_loc_vals+", similarity = "+str(similarity))
+            location_dict["latitude"] = nominatim_result.latitude
+            location_dict["longitude"] = nominatim_result.longitude
+            
+            #Reverse geocoding to correct the spelling
+            time_new_request = time.time()
+            if time_new_request - time_last_request <= 1:
+                time.sleep(1)#time_new_request - time_last_request)
+            location_details = geolocator.reverse((nominatim_result.latitude, nominatim_result.longitude), exactly_one=True, addressdetails=True, language="en")
+            time_last_request = time_new_request
+            address = location_details.raw.get("address", {})
+
+            for loc_level in locations_levels :
+                if loc_level in address.keys() : 
+                    location_dict[loc_level] = address.get(loc_level)
+                else : 
+                    location_dict[loc_level] = None            
+            return location_dict, time_last_request
+        else :
+            if print_info :
+                print("WRONG location "+nominatim_result.raw['name']+" for "+finest_loc_vals+", similarity = "+str(similarity))
+            return None, time_last_request
+
+def geocoding_impact_location(df, print_info=False, locations_levels=['country', 'region', 'state', 'city']) :
+    """
+    Use this function if the input df has only the columns ['location'] to describe the location
+    Finer location can be added such as : 
+    - village 
+    - suburb
+    - road
+    """
+    time_last_request=time.time()
+    df = df.replace({np.nan: None})
+    columns_input = list(filter(lambda x: (x != "country")&(x != "location"), df.columns.tolist()))
+    df_geo = pd.DataFrame(columns=columns_input+['latitude', 'longitude']+locations_levels)
+    for index, row in df.iterrows():
+        country = row["country"]
+        #Check if precise location informations are available
+        if row.location:
+            for col in locations_levels+['latitude', 'longitude'] :
+                row[col] = []
+            for loc in row.location : 
+                location_dict, time_last_request = geocode_single_location(loc, locations_levels, time_last_request, print_info=False, similarity_th=0.4)
+                if location_dict : 
+                    for col in locations_levels+['latitude', 'longitude'] : 
+                        row[col].append(location_dict[col])
+            #Kepp only the set of unique occurence 
+            for col in locations_levels+['latitude', 'longitude'] :
+                row[col] = list(set(row[col])) if row[col] else None
+            df_geo = pd.concat([df_geo, row.to_frame().T], ignore_index=True)
+        #Otherwise work with country level
+        else:
+            location_dict, time_last_request = geocode_single_location(row.country, locations_levels, time_last_request, print_info=False, similarity_th=0.4)
+            if location_dict : 
+                for col in locations_levels+['latitude', 'longitude'] : 
+                    row[col] = location_dict[col]
+            df_geo = pd.concat([df_geo, row.to_frame().T], ignore_index=True)
     return df_geo
