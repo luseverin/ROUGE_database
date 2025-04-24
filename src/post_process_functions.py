5ff66677cef28fbecaa70a093b072b85e173e353
luseverin <luca.severino@usys.ethz.ch>
2025-04-24 17:11:06 +0200

    Add function to post process dates

--- src/post_process_functions.py
+++ src/post_process_functions.py
@@ -42,9 +42,9 @@ def convert_labelled_chat_format(df_labelled) :
             #Add region
             if isinstance(row.region, str)  :
                 regions = ast.literal_eval(row.region)
-            elif isinstance(row.region, list) : 
+            elif isinstance(row.region, list) :
                 regions = row.region
-            else : 
+            else :
                 regions = None
 
             if not regions is None:
@@ -52,7 +52,7 @@ def convert_labelled_chat_format(df_labelled) :
                     row_append = row.copy()
                     row_append['region'] = region
                     row_append['city'] = None
-    
+
                     #Find the corresponding locationAnnotation
                     # print(row)
                     # print(row.locationAnnotation)
@@ -67,15 +67,15 @@ def convert_labelled_chat_format(df_labelled) :
                 cities = ast.literal_eval(row.city)
             elif isinstance(row.city, list) :
                 cities = row.city
-            else : 
+            else :
                 cities = None
-                
-            if not cities is None : 
+
+            if not cities is None :
                 for city in cities :
                     row_append = row.copy()
                     row_append['region'] = None
                     row_append['city'] = city
-    
+
                     #Find the corresponding locationAnnotation
                     locationsAnnotations = ast.literal_eval(row.locationAnnotation)
                     for locationAnnot in locationsAnnotations :
@@ -90,15 +90,15 @@ def clean_chat_format(df_labelled) :
     """
     df_labelled_chat = pd.DataFrame(columns=df_labelled.columns)
     for index, row in df_labelled.iterrows():
-        #Loop over the hazardSubtype 
+        #Loop over the hazardSubtype
         hazardsubtypes = ast.literal_eval(row.hazardSubtypes)
         if not hazardsubtypes :
             hazardsubtypes = [None]
-        for subtype in hazardsubtypes : 
+        for subtype in hazardsubtypes :
             #If no regions and cities
             if isinstance(row.region, float) and isinstance(row.city, float) :
                 df_labelled_chat = pd.concat([df_labelled_chat, row.to_frame().T], ignore_index=True)
-    
+
             else :
                 regions = row.region
                 if not regions is None :
@@ -108,10 +108,10 @@ def clean_chat_format(df_labelled) :
                         row_append['city'] = None
                         row_append['hazardSubtypes'] = subtype
                         df_labelled_chat = pd.concat([df_labelled_chat, row_append.to_frame().T], ignore_index=True)
-    
+
                 #Add location
                 cities = row.city
-                if not cities is None : 
+                if not cities is None :
                     for city in cities :
                         row_append = row.copy()
                         row_append['region'] = None
@@ -119,7 +119,38 @@ def clean_chat_format(df_labelled) :
                         row_append['hazardSubtypes'] = subtype
                         df_labelled_chat = pd.concat([df_labelled_chat, row_append.to_frame().T], ignore_index=True)
     return df_labelled_chat
-    
+
+def make_date(report_df):
+    """
+    Make start and end dates from the separate columns in the report DataFrame.
+
+    Parameters
+    ----------
+    report_df : DataFrame
+        The DataFrame containing the report data.
+
+    Returns
+    -------
+    DataFrame
+        The DataFrame with the new columns "startDate" and "endDate".
+    """
+    def get_date(df, prefix):
+        year = df[f'{prefix}Year']
+        month = df[f'{prefix}Month']
+        day = df[f'{prefix}Day']
+
+        if pd.isna(year) or year is None:
+            return ""
+        elif pd.isna(month) or month is None:
+            return f"{int(df[f'{prefix}Year'])}"
+        elif pd.isna(day) or day is None:
+            return f"{int(df[f'{prefix}Year'])}-{int(df[f'{prefix}Month'])}"
+        return f"{int(df[f'{prefix}Year'])}-{int(df[f'{prefix}Month'])}-{int(df[f'{prefix}Day'])}"
+
+    report_df["startDate"] = report_df.apply(lambda x: get_date(x, prefix='start'), axis=1)
+    report_df["endDate"] = report_df.apply(lambda x: get_date(x, prefix='end'), axis=1)
+    return report_df
+
 ## DEPRECATED
 ## Convert json to dataframe
 #def clean_output_df(results_json, out_cols=['Hazard','Country','Location','Start_Date','End_Date']):
