5bf5fcd646061a02fa68f0dffb3e60a0228ae7b3
luseverin <luca.severino@usys.ethz.ch>
2025-05-20 16:33:50 +0200

    Separate definition of hazards and impacts in different files

--- src/impact_def.py
+++ src/impact_def.py
@@ -1,52 +1,13 @@
-#hazard subtypes directly taken from EMDAT
-hazard_all_subtype_emdat = """["drought", "wildfire", "forest fire", "land fire", "ground movement", "tsunami", "avalanche", "landslide", "rockfall", "sudden subsidence", "mudslide", "ash fall", "lava flow", "pyroclastic flow", "lahar", "coastal flood", "flash flood", "riverine flood", "ice jam flood", "rogue wave", "seiche", "coldwave", "heatwave", "severe winter conditions", "derecho", "hail", "lightning", "winterstorm", "storm surge", "tornado", "extra-tropical storm", "tropical storm" ]"""
+#Constants for impact definition
 
-#dict to match emdat maintypes to there subtypes
-maintype_to_subytpe_emdat = {'Drought': ['drought'],
- 'Wildfire': ['wildfire', 'forest fire', 'land fire'],
- 'Earthquake': ['ground movement', 'tsunami'],
- 'Mass movement': ['avalanche',
-  'landslide',
-  'rockfall',
-  'sudden subsidence',
-  'mudslide'],
- 'Volcanic activity': ['ash fall', 'lava flow', 'pyroclastic flow', 'lahar'],
- 'Flood': ['\\b(coastal flood',
-  'flash flood',
-  'riverine flood',
-  'ice jam flood)\\b'],
- 'Wave action': ['rogue wave', 'seiche'],
- 'Extreme temperature': ['coldwave', 'heatwave', 'severe winter conditions'],
- 'Storm': ['derecho',
-  'hail',
-  'lightning',
-  'winterstorm',
-  'storm surge',
-  'tornado',
-  'winter storm',
-  'extra-tropical storm',
-  'tropical storm']}
 
-hazard_subtype_kw_searc ={
-                        'Drought': r"drought.*|dry spell.*",
-                        'Wildfire': r"fire.*|forestfire.*|wildfire.*|landfire.*|bushfire.*|forest fire.*|wild fire.*|land fire.*|bush fire.*" ,
-                        'Earthquake' : r"ground movement.|tsunami.",
-                        'Mass movement': r"avalanche.|land slide.*|landslide.*|rockfall.*|sudden subsidence.|mudslide.|mass movement.*",
-                        'Volcanic activity' : r"ash fall.|lava flow.|pyroclastic flow.|lahar",
-                        'Flood': r"flood.*|inundation.*|coastal flood.|flash flood.|riverine flood.|ice jam flood.",
-                        'Wave action' : r"rogue wave.|seiche",
-                        'Extreme temperature' : r"cold wave.*|coldwave.*|cold spell.*|heat wave.*|heatwave.*|heat episode.*|((heat|hot) spell).*|heat stress.*|severe winter conditions.",
-                        'Storm' : r"derecho.|hail.|lightning.|winterstorm.|storm surge.|tornado.|winter storm.|extra-tropical storm.|tropical storm.|typhoon.|hurricane.|storm.*|superstorm.*|windstorm.*|snowstorm.*|blizzard.*|thunderstorm.*" }
-
-#impact types dict
-impact_types_dict = {
-    "infrastructures": ["roads", "healthcare", "hospitals", "schools", "powerplants", "bridges"],
-    "population" : ["displaced", "affected", "deaths", "injured"]
-
-}
 impact_cat_list = ["Human Impacts","Transportation Infrastructure",
                    "Healthcare Infrastructure","IT and Communication Infrastructure",
                    "Residential Buildings","Informal Settlements","Education Infrastructure"]
+
+impactSubtype_list = ["Affected People", "Injured People", "Displaced People", "Homeless People", "Missing People", "Human Deaths", "Affected Families and Households"
+                      "Transportation Infrastructure", "Water, Sanitation, and Hygiene Infrastructure", "Healthcare Infrastructure", "IT and Communication Infrastructure", "Residential Buildings", "Informal settlements", "Education Infrastructure"]
+
 impact_cat_desc_dict = {
     "Human impacts": "The impacts on the human population resulting from the natural hazard event. Look for words such as 'Affected People', 'Injured People', 'Displaced People','Homeless People', 'Missing People', 'Human Deaths'.",
     "Transportation Infrastructure": "The impacts on the transportation infrastructures resulting from the natural hazard event. Look for words such as 'roads', 'bridges', 'railways', and 'highways'.",
@@ -72,6 +33,23 @@ impact_subtypes_desc_dict = {
         "Residential Buildings": "Residential buildings impacted by a natural hazard event.",
         "Informal Settlements":"Informal settlements such as refugee camps, slums, tents, etc. impacted by a natural hazard event.",
         "Education Infrastructure": "Education infrastructure such as schools, universities, etc. impacted by a natural hazard event.",
+        }
+
+impact_subtypes_unit_dict = {
+        "Affected People": ["people"],
+        "Injured People": ["people"],
+        "Displaced People": ["people"],
+        "Homeless People": ["people"],
+        "Missing People": ["people"],
+        "Human Deaths": ["people"],
+        "Blocked roads" : ["km", "nb."],
+        "Airports": ["nb."],
+        "WASH infrastructure": ["nb."],
+        "Healthcare Infrastructure": ["nb."],
+        "IT and Communication Infrastructure": ["nb."],
+        "Residential Buildings": ["nb."],
+        "Informal Settlements": ["nb."],
+        "Education Infrastructure": ["nb."],
         }
 
 impact_subtypes_desc_quant_dict = {
@@ -106,12 +84,4 @@ impact_subtypes_desc_quant_dict = {
         }
     }
 
-impactSubtype_list = ["Affected People", "Injured People", "Displaced People", "Homeless People", "Missing People", "Human Deaths", "Affected Families and Households"
-                      "Transportation Infrastructure", "Water, Sanitation, and Hygiene Infrastructure", "Healthcare Infrastructure", "IT and Communication Infrastructure", "Residential Buildings", "Informal settlements", "Education Infrastructure"]
 
-#Countries
-import pycountry
-import re
-unique_countries_ISO = [country.alpha_3 for country in pycountry.countries]
-unique_country_names = [country.name for country in pycountry.countries]
-pattern_country = '|'.join(map(re.escape, unique_country_names))
