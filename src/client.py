89283073124169d161bf3abe1a29baa99ba1471d
laurahasbini <laura.hasbini@ensta-paris.fr>
2025-05-23 14:04:56 +0200

    Specify the API key per user

--- src/client.py
+++ src/client.py
@@ -1,10 +1,12 @@
 #set openai api
 from openai import OpenAI
 import instructor
+import getpass
 
 global CLIENT
 global MODEL_NAME
 MODEL_NAME = "mistral-saba-24b"
+user = getpass.getuser()
 ## Examples of models with Groq :
 MODEL_NAME_LIST = [
     "llama3-70b-8192",
@@ -35,8 +37,10 @@ if MODEL_NAME in ["gpt-4o-mini", "gpt-3.5-turbo-0125"] :
     API_KEY = os.getenv("GROQ_API_KEY")
     CLIENT = OpenAI(api_key=API_KEY)
 else :
-    API_KEY = os.getenv("GROQ_API_KEY")
-    #"os.getenv("GROQ_API_KEY")" Laura's
+    if user == "lhasbini" : 
+        API_KEY = os.getenv("GROQ_API_KEY")
+    elif user == "lseverino" : 
+        API_KEY = os.getenv("GROQ_API_KEY")
     CLIENT = OpenAI(
         api_key=API_KEY,
         base_url="https://api.groq.com/openai/v1"  # <-- Groq’s OpenAI-compatible endpoint
