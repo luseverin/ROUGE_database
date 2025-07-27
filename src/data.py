3ba6bcdaa5690aac8ebef05f65955c8c01573279
luseverin <luca.severino@usys.ethz.ch>
2025-07-27 11:55:36 +0200

    Add folder path for post proc data

--- src/data.py
+++ src/data.py
@@ -4,14 +4,15 @@ DATA_PATH_LS = '../Data_backup/'
 DATA_PATH_LH = "/scratchx/lhasbini/como_school/"
 
 user = getpass.getuser()
-if user == "lhasbini" : 
+if user == "lhasbini" :
     DATA_PATH = DATA_PATH_LH
 elif user == "lseverino" : ##### TO CHANGE FOR LUCA
     DATA_PATH = DATA_PATH_LS
 else:
     raise ValueError(f"Unknown user: {user}")
-    
+
 DATA_IN_JSONS = DATA_PATH + 'report_jsons/'
 DATA_OUT_LLMS = DATA_PATH + 'results_llm/'
 DATA_LABELLED = DATA_PATH + 'labelled/'
 DATA_FIGURE = DATA_PATH + 'figure/'
+DATA_OUT_PROC = DATA_PATH + 'results_proc/'
