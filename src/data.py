19e8e2ca479b36fc80bb24f93d8c30b98c33a980
laurahasbini <laura.hasbini@lsce.ipsl.fr>
2025-08-19 13:42:17 +0200

    Add path for external impact sources

--- src/data.py
+++ src/data.py
@@ -1,7 +1,8 @@
 ##paths and variables
 import getpass
 DATA_PATH_LS = '../Data_backup/'
-DATA_PATH_LH = "/scratchx/lhasbini/como_school/"
+# DATA_PATH_LH = "/scratchx/lhasbini/como_school/"
+DATA_PATH_LH = "c:/Users/lhasbini/ownCloud/Documents/Thèse/Conférences_Discussions/2024_Como_Compound Events Training/data/"
 
 user = getpass.getuser()
 if user == "lhasbini" : 
@@ -15,4 +16,5 @@ DATA_IN_JSONS = DATA_PATH + 'report_jsons/'
 DATA_OUT_LLMS = DATA_PATH + 'results_llm/'
 DATA_LABELLED = DATA_PATH + 'labelled/'
 DATA_FIGURE = DATA_PATH + 'figure/'
+DATA_EXTERNAL_SOURCE = DATA_PATH + 'external_impact/'
 ADMIN_PATH = DATA_PATH+"admin_files/"
