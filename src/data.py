150624fd967e23944e576c85a56a3f8634691eae
luseverin <luca.severino@usys.ethz.ch>
2025-09-10 16:21:23 +0200

    Use pathlib for datapaths

--- src/data.py
+++ src/data.py
@@ -1,8 +1,9 @@
 ##paths and variables
 import getpass
-DATA_PATH_LS = '../Data_backup/'
+from pathlib import Path
+DATA_PATH_LS = Path('../Data_backup/')
 # DATA_PATH_LH = "/scratchx/lhasbini/como_school/"
-DATA_PATH_LH = "c:/Users/lhasbini/ownCloud/Documents/Thèse/Conférences_Discussions/2024_Como_Compound Events Training/data/"
+DATA_PATH_LH = Path("c:/Users/lhasbini/ownCloud/Documents/Thèse/Conférences_Discussions/2024_Como_Compound Events Training/data/")
 
 user = getpass.getuser()
 if user == "lhasbini" :
@@ -12,10 +13,10 @@ elif user == "lseverino" : ##### TO CHANGE FOR LUCA
 else:
     raise ValueError(f"Unknown user: {user}")
 
-DATA_IN_JSONS = DATA_PATH + 'report_jsons/'
-DATA_OUT_LLMS = DATA_PATH + 'results_llm/'
-DATA_LABELLED = DATA_PATH + 'labelled/'
-DATA_FIGURE = DATA_PATH + 'figure/'
-DATA_OUT_PROC = DATA_PATH + 'results_proc/'
-DATA_EXTERNAL_SOURCE = DATA_PATH + 'external_impact/'
-ADMIN_PATH = DATA_PATH+"admin_files/"
+DATA_IN_JSONS = DATA_PATH / 'report_jsons/'
+DATA_OUT_LLMS = DATA_PATH / 'results_llm/'
+DATA_LABELLED = DATA_PATH / 'labelled/'
+DATA_FIGURE = DATA_PATH / 'figure/'
+DATA_OUT_PROC = DATA_PATH / 'results_proc/'
+DATA_EXTERNAL_SOURCE = DATA_PATH / 'external_impact/'
+ADMIN_PATH = DATA_PATH / "admin_files/"
