66f2c22399a09045bdf153311b08df3d13228033
luseverin <luca.severino@usys.ethz.ch>
2025-09-16 11:06:36 +0200

    Update impact desc and make different impact desc consistent

--- src/impact_def.py
+++ src/impact_def.py
@@ -2,10 +2,10 @@
 
 
 impactType_list = ["Human",
-"Service Access",
-"Infrastructure",
-"Agriculture",
-"Economic Activity & Livelihood Production"]
+                   "Service Access",
+                   "Infrastructure",
+                   "Agriculture",
+                   "Economic Activity & Livelihood Production"]
 
 
 impactSubtype_list = ["Affected People", "Injured People", "Displaced People", "Homeless People", "Missing People", "Human Deaths", "Human Health and Wellbeing", "Infected and Ill People",#Affected Families and Households"
@@ -17,33 +17,54 @@ impactSubtype_list = ["Affected People", "Injured People", "Displaced People", "
                       #"Health Impacts"]
 
 impact_subtypes_desc_dict = {
-        "Affected People": "People impacted by a natural hazard event (the term affected must be mentioned). Reports the impacts in number of people.",
-        "Injured People": "People injured, including those hospitalized or admitted (the term injured must be used), to a natural hazard event. Reports the impacts in number of people.",
-        "Displaced People": "People forcefully displaced or evacuated before or following the event. Reports the impacts in number of people.",
-        "Homeless People": "People who lost their homes, to a natural hazard event. Reports the impacts in number of people.",
-        "Missing People": "People unaccounted for following a natural hazard event. Reports the impacts in number of people.",
-        "Human Deaths": "Fatalities caused by a natural hazard event. Reports the impacts in number of deaths.",
-        "Human Health and Wellbeing": "The impacts on human health and wellbeing such as infection, illness, etc. resulting from a natural hazard event. Reports the impacts in number of people.",
-        "Roads" : "Roads (including bridges and highways) blocked or damaged or disrupted traffic due to a natural hazard event. Reports the impacts in number of , kilometers of, or damage cost of roads or bridges affected.",
-        "Other Transportation Infrastructure": "The impacts on the transportation infrastructures other than roads such as airports, railways, etc. resulting from the natural hazard event. Reports the impacts in number of or kilometers of or damage cost of facilities affected.",
-        "Water, Sanitation, and Hygiene Infrastructure": "Water, sanitation, and hygiene infrastructure such as sewage networks, drainage systems, wastewater treatment plants, etc. impacted by a natural hazard event.  Reports the impacts in number of or damage cost of facilities affected.",
-        "Healthcare Infrastructure": "Healthcare infrastructure such as hospitals, healthcare centers, pharmacies, clinics, etc. impacted by a natural hazard event. Reports the impacts in number of or damage cost of facilities affected.",
-        "IT and Communication Infrastructure": "IT and communication infrastructure such as data centers, communication towers, and cables impacted by a natural hazard event. Reports the impacts in number of or damage cost of facilities affected.",
-        "Residential Buildings": "Residential buildings impacted by a natural hazard event. Reports the impacts in number of or damage cost of buildings affected.",
-        "Informal Settlements":"Informal settlements such as refugee camps, slums, tents, etc. impacted by a natural hazard event. Reports the impacts in number of or damage cost of settlements affected.",
-        "Education Infrastructure": "Education infrastructure such as schools, universities, etc. impacted by a natural hazard event. Reports the impacts in number of or damage cost of facilities affected.",
-        "Agriculture Infrastructure": "Agriculture infrastructures such as farms, silos, irrigation systems, greenhouses, etc. impacted by a natural hazard event. Reports the impacts in number of or damage cost of facilities affected.",
-        "Power and Energy Production Infrastructure": "Power and energy production infrastructures such as power plants, grids, wind farms, solar farms, etc. impacted by a natural hazard event. Reports the impacts in number of or damage cost of facilities affected.",
-        "Crop Production and Forestry": "The impacts on crop production and forestry such as trees, crops, kg of crops, etc. resulting from a natural hazard event. Reports the impacts in number of, kilometer ** 2 of, kilograms of, or damage cost of crops or trees affected.",
-        "Affected Livestock and Animals": "The impacts on livestock and animals such as cattles, pigs, sheeps, fishes, etc. impacted by a natural hazard event. Reports the impacts in number of or damage cost of livestock or animals affected.",
-        "Economy and Market": "Changes in prices or market instability resulting from a natural hazard event. Reports the impacts in percentage of price changes.",
-        "Recreation, Tourism, and Culture": "The impacts on recreation, tourism, and culture such as cultural heritage sites, tourist attractions, etc. resulting from a natural hazard event. Reports the impacts in number of or damage cost of facilities affected.",
-        "Access to Healthcare": "People losing access to healthcare resulting from a natural hazard event. Reports the impacts in number of people losing access to healthcare.",
-        "Access to transport and Mobility": "People losing access to transport or being stranded resulting from a natural hazard event. Reports the impacts in number of people losing access to transport or being stranded.",
-        "Water Quality and Availability": "Changes in the quality and availability of water resulting from a natural hazard event. Reports the impacts in percentage of water quality changes.",
-        "Access to Education": "People losing access to education resulting from a natural hazard event. Reports the impacts in number of people losing access to education.",
-        "Access to Power and Energy": "People losing access to power or energy resulting from a natural hazard event. Reports the impacts in number of people losing access to power or energy.",
-        }
+    #"Human":
+    #The impacts on the human population resulting from the hazards.
+    "Affected People": "Total number of individuals impacted by the hazard event.",
+    "Injured People": "Number of people injured, including those hospitalised or admitted.",
+    "Displaced People": "Number of people forcefully displaced or evacuated before or following the event.",
+    "Homeless People": "Number of people losing housing following the event.",
+    "Missing People": "Number of people unaccounted for following the event.",
+    "Human Deaths": "Number of fatalities caused by the hazard.",
+    "Infected and Ill People" : "Number of people contaminated (cases) by an infectious disease." ,
+    "Human Health and Wellbeing" : "Generic impacts on human health (physical, mental) or wellbeing not directly associated with the spread of an infectious disease.",
+    "Other Human Impacts" : "Any identified impact on humans which cannot be associated with the previous impactSubtypes.",
+
+    #"Infrastructure":
+    #The impacts on the built infrastructure resulting from the hazards.
+    "Road Infrastructure" : "Roads and road infrastructure (e.g. bridges, highways..) impacted by a hazard.",
+    "Other Transportation Infrastructure": "Transportation infrastructure, other than roads, impacted by a hazard. For example : Railways, airport, ferries, cars…",
+    "Water, Sanitation, and Hygiene Infrastructure": "Number of water, sanitation, and hygiene infrastructure such as sewage networks, drainage systems, wastewater treatment plants, etc. impacted by a hazard.",
+    "Healthcare Infrastructure": "Number of healthcare infrastructure such as hospitals, healthcare centers, pharmacies, clinics, etc.  impacted by a hazard.",
+    "IT and Communication Infrastructure": "Number of IT and communication infrastructure  such as data centers, communication towers, and cables impacted by a hazard.",
+    "Residential Buildings": "Number of residential buildings impacted by a hazard. This category also encompasses the impact on houses.",
+    "Informal settlements": "Number of informal settlements such as refugee camps, slums, tents, etc. impacted by a hazard.",
+    "Education Infrastructure": "Number of education infrastructures such as schools, universities, etc. impacted by a hazard.",
+    "Power and Energy Production Infrastructure": "Number of energy production infrastructures such as power plants, turbines, grids, pipelines, etc., impacted by a hazard.",
+    "Agricultural Infrastructure": "Number of agricultural infrastructures such as farms, warehouses, greenhouses, etc. impacted by a hazard. This category should also gather impacted infrastructure for fisheries such as vessels, boats, etc.",
+    "Other Infrastructural Impacts" : "Any identified impact on infrastructure which cannot be associated with the previous impactSubtypes, e.g. dykes, embankment, shops, etc.",
+
+    #"Service access":
+    #The impacts on the provision of basic services to the population resulting from the hazards.
+    "Access to Healthcare" : "People losing the ability to obtain needed medical services, including preventative care, emergency services…",
+    "Access to Food" : "People losing access to a secure food supply.",
+    "Mobility and Access to Transport" : "People losing the capacity to move safely and efficiently between locations.",
+    "Access to Water, Sanitation, and Hygiene" : "People losing access to water, sanitation, and hygiene services.",
+    "Water Quality and Availability" : "People losing safe, clean, and consistent supply of water for drinking, sanitation, and other essential uses.",
+    "Access to Education" : "People losing the ability to attend educational institutions and receive instruction.",
+    "Access to Power and Energy" :  "People losing access to electricity, gas, or other energy sources necessary for households, businesses, and public services.",
+    "Other Service Access Impacts" : "Any identified impact on service access which cannot be associated with the previous impactSubtypes.",
+
+    #"Agriculture":
+    #The impacts on agriculture resulting from the hazards.
+    "Crop Production and Forestry" : "Number of crops, agricultural production and forest impacted by a hazard.",
+    "Affected Livestock and Animals" : "Total number of animals, including terrestrial and aquatic species, impacted by the hazard (e.g. loss, death, perished animals or fishes).",
+    "Other Agricultural Impacts" : "Any identified impact on agriculture which cannot be associated with the previous impactSubtypes.",
+
+    #"Economic Activity & Livelihood Production":
+    #The impacts on the economy and livelihood resulting from the hazards.
+    "Recreation, Tourism, and Culture" : "Tourist attractions and cultural sites impacted by a hazard.",
+    "Other Economic Activity & Livelihood Production" : "Any identified impact on the economy which cannot be associated with the previous impactSubtypes.",
+}
 
 impact_kw_reclass = {
     'Affected People': r"\bAffected People\b",
@@ -55,6 +76,7 @@ impact_kw_reclass = {
     'Human Health and Wellbeing': r"\bHuman Health and Wellbeing\b",
     'Infected and Ill People': r"\bInfected and Ill People\b",
 
+
     'Road Infrastructure': r"\b(Road Infrastructure|road)s?\b",
     'Other Transportation Infrastructure': r"\bOther Transportation Infrastructure\b",
     'Water, Sanitation, and Hygiene Infrastructure': r"\bWater,?\s*Sanitation,?\s*and Hygiene Infrastructure\b",
@@ -65,25 +87,24 @@ impact_kw_reclass = {
     'Informal settlements': r"\bInformal settlements\b",
     'Education Infrastructure': r"\bEducation Infrastructure\b",
     'Power and Energy Production Infrastructure': r"\bPower and Energy Production Infrastructure\b",
-    'Agriculture Infrastructure': r"\bAgricultur(?:e|al)? Infras(?:tructure|tucture)\b",
+    'Agricultural Infrastructure': r"\bAgricultur(?:e|al)? Infras(?:tructure|tucture)\b",
 
     'Crop Production and Forestry': r"\bCrop Production and Forestry\b",
     'Affected Livestock and Animals': r"\bAffected Livestock and Animals\b",
 
-    'Other Economic and Livelihood Impacts': r"\b(Other Economic(?: Activity)? (?:and|&) Livelihood (?:Production|Impact(?:s)?)|Economy and Market|Livelihood|employment|basic needs)\s*\b",
+    'Other Economic Activity & Livelihood Production': r"\b(Other Economic(?: Activity)? (?:and|&) Livelihood (?:Production|Impact(?:s)?)|Economy and Market|Livelihood|employment|basic needs)\s*\b",
     'Recreation, Tourism, and Culture': r"\bRecreation, Tourism, and Culture\b",
 
     'Access to Healthcare': r"\bAccess to Healthcare\b",
-    'Access to transport and Mobility': r"\b(Access to transport and Mobility|Access to transport|Mobility)\b",
+    'Mobility and Access to Transport': r"\b(Access to transport and Mobility|Access to transport|Mobility)\b",
     'Water Quality and Availability': r"\bWater Quality and Availability\b",
     'Access to Education': r"\bAccess to Education\b",
     'Access to Power and Energy': r"\bAccess to Power and Energy\b",
     'Access to Food': r"\bfood\b",
     'Access to Water, Sanitation, and Hygiene': r"\bAccess to Water,?\s*Sanitation,?\s*and Hygiene\b",
 
-    'Other Infrastructure Impacts': r"\b(Other Infrastructur(?:e|al)? Impacts?)\b",
+    'Other Infrastructural Impacts': r"\b(Other Infrastructur(?:e|al)? Impacts?)\b",
     'Other Human Impacts': r"\bOther Human.* Impacts?\b",
-    'Other Environmental Impacts': r"\bOther Environmental.* Impacts?\b",
     'Other Service Access Impacts': r"\bOther Service Access(?: Impacts?)?\b",
     'Other Agricultural Impacts': r"\bOther Agricultural.* Impacts?\b",
 }
