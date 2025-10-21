0380cf2ee487be515bf3feb2eeb6b3cf1c644ac2
luseverin <luca.severino@usys.ethz.ch>
2025-10-21 17:06:06 +0200

    Move filtering funcs to utils

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,19 @@
 def print_match(matched_df):
     """Display function to better visualize match df"""
     return matched_df[["appealCode","impactSubtype", "impactSubtype_matched", "impactValue", "impactValue_matched", "impactUnit", "impactUnit_matched", "location", "location_matched", "impactSubtype_sim", "impactUnit_sim", "geometry_sim", "startYear_sim", "startMonth_sim", "startDay_sim","endYear_sim","endMonth_sim", "endDay_sim","hazards_sim","match_sim","valueAnnotation", "annotation_matched"]]
+
+def filter_by_flags(df, flag_filters=list):
+    for flt_flag in flag_filters:
+        df = df[~df[flt_flag]]
+    return df
+
+def filter_matches(matched_df, value_error_th=0.05, sim_th=0.6, match_cat=["impactSubtype"]):
+    """Filter matches based on value error threshold for quantitative and similarity threshold for qualitative"""
+    #filter matches
+    matched_df_filter_qt = matched_df.copy()
+    matched_df_filter_qt = matched_df_filter_qt[(matched_df_filter_qt["match_sim"] >= sim_th) & (matched_df_filter_qt["impactValue_error"] <= value_error_th) & (matched_df_filter_qt["quanti"]=="quanti")]
+    matched_df_filter_ql = matched_df.copy()
+    matched_df_filter_ql = matched_df_filter_ql[(matched_df_filter_ql["match_sim"] >= sim_th) & (matched_df_filter_ql["quanti"]=="quali")]
+    if match_cat:
+        matched_df_filter_ql = pd.concat([matched_df_filter_ql.query(f"{cat} == {cat}_matched").dropna(how="all",axis=0) for cat in match_cat]).sort_index()
+    return pd.concat([matched_df_filter_qt, matched_df_filter_ql], axis=0)
