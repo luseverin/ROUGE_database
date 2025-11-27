618ac2c54af1cfa2c245a18afb7c65345907562d
luseverin <luca.severino@usys.ethz.ch>
2025-11-27 11:37:03 +0100

    Some cleanup in client setup

--- src/client.py
+++ src/client.py
@@ -1,60 +1,59 @@
-#set openai api
+import requests
+import pandas as pd
 from openai import OpenAI
 import instructor
 import getpass
 
-global CLIENT
-global MODEL_NAME
-MODEL_NAME = "meta-llama/llama-4-scout-17b-16e-instruct"#"meta-llama/llama-4-scout-17b-16e-instruct"
-MAX_COMPLETION_TOKENS = 8192
-CONTEXT_WINDOW = 131072
-## Examples of models with Groq :
-MODEL_NAME_LIST = [
-    "openai/gpt-oss-20b",
-    "llama3-70b-8192",
-    "llama3-8b-8192",
-    "mistral-saba-24b",
-    "llama-3.1-8b-instant",
-    "playai-tts",
-    "meta-llama/llama-4-maverick-17b-128e-instruct",
-    "deepseek-r1-distill-llama-70b",
-    "qwen-qwq-32b",
-    "whisper-large-v3",
-    "llama-guard-3-8b",
-    "distil-whisper-large-v3-en",
-    "llama-3.3-70b-versatile",
-    "allam-2-7b",
-    "gemma2-9b-it",
-    "compound-beta-mini",
-    "playai-tts-arabic",
-    "meta-llama/llama-4-scout-17b-16e-instruct",
-    "meta-llama/llama-guard-4-12b",
-    "compound-beta",
-    "whisper-large-v3-turbo",
-    "meta-llama/llama-prompt-guard-2-22m",
-    "meta-llama/llama-prompt-guard-2-86m",
-    "qwen-qwq-32b",
-    "qwen/qwen3-32b"
-    "moonshotai/kimi-k2-instruct"
+##set groq api
+#set up api key
+API_KEY = os.getenv("GROQ_API_KEY")
+#"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" #Luca's
+#"os.getenv("GROQ_API_KEY")" Laura's
 
-]
-
-
-
-if MODEL_NAME in ["gpt-4o-mini", "gpt-3.5-turbo-0125"] :
-    API_KEY = os.getenv("GROQ_API_KEY")
-    CLIENT = OpenAI(api_key=API_KEY)
-else :
-    API_KEY = os.getenv("GROQ_API_KEY")
-    #"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" #Luca's
-    #"os.getenv("GROQ_API_KEY")" Laura's
-    CLIENT = OpenAI(
+#set up client
+CLIENT = OpenAI(
         api_key=API_KEY,
         base_url="https://api.groq.com/openai/v1"  # <-- Groq’s OpenAI-compatible endpoint
     )
-
 # Enables `response_model`
 CLIENT = instructor.patch(client=CLIENT)
+
+#get model list
+url = "https://api.groq.com/openai/v1/models"
+headers = {
+    "Authorization": f"Bearer {API_KEY}"
+}
+response = requests.get(url, headers=headers)
+model_table = pd.DataFrame(response.json()["data"])
+# Examples of models with Groq (from model table):
+MODEL_NAME_LIST = ['playai-tts-arabic',
+                   'allam-2-7b',
+                   'qwen/qwen3-32b',
+                   'meta-llama/llama-prompt-guard-2-22m',
+                   'meta-llama/llama-4-scout-17b-16e-instruct',
+                   'meta-llama/llama-prompt-guard-2-86m',
+                   'meta-llama/llama-4-maverick-17b-128e-instruct',
+                   'whisper-large-v3-turbo',
+                   'moonshotai/kimi-k2-instruct-0905',
+                   'playai-tts',
+                   'meta-llama/llama-guard-4-12b',
+                   'moonshotai/kimi-k2-instruct',
+                   'groq/compound-mini',
+                   'llama-3.3-70b-versatile',
+                   'openai/gpt-oss-20b',
+                   'llama-3.1-8b-instant',
+                   'groq/compound',
+                   'openai/gpt-oss-120b',
+                   'openai/gpt-oss-safeguard-20b',
+                   'whisper-large-v3']
+
+#seelct model
+MODEL_NAME = 'llama-3.3-70b-versatile'
+
+#retrieve max tokens
+MAX_COMPLETION_TOKENS = model_table[model_table["id"] == MODEL_NAME]["max_completion_tokens"].values[0]
+CONTEXT_WINDOW = model_table[model_table["id"] == MODEL_NAME]["context_window"].values[0]
+
 
 ## Nominatim
 user = getpass.getuser()
