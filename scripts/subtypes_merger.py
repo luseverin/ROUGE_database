5a2c21e4bca1cf191acb4bac8a9b2fe6dfb8c9f1
luseverin <luca.severino@usys.ethz.ch>
2025-12-23 15:06:27 +0100

    Remove useless code in subtype_merger

--- scripts/subtypes_merger.py
+++ scripts/subtypes_merger.py
@@ -7,21 +7,18 @@ from src.impact_def import IMPACT_SUBTYPE_MERGER
 from src.logger_setup import set_logger
 
 ## Parameters
-dedup_cols = ["appealCode","impactSubtype", "impactValue", "impactUnit", "country",
-              "location", "startYear", "startMonth", "startDay","endYear", "endMonth",
-              "endDay", "hazards"]
 
 ##load data (model)
-res_savename = "post_processed_all_appeals_longest_1-717_meta-llama_llama-4-scout-17b-16e-instruct_v121225"
+res_savename = "filter_post_processed_all_appeals_longest_1-717_meta-llama_llama-4-scout-17b-16e-instruct_v121225_geo_v231225"
 #"post_processed_labelled_reports_fixed_impact_desc_meta-llama_llama-4-scout-17b-16e-instruct_v271025"
 #"post_processed_new_unit_std_labelled_reports_impacts_all_v111025"
 suffix = "_geo_v171225"
-res_savename_geo = res_savename + suffix
+res_savename_geo = res_savename
 response_df = pd.read_csv(DATA_OUT_PROC / (res_savename + ".csv"))
 response_df_geo = gpd.read_file(DATA_OUT_PROC / (res_savename_geo+".gpkg"))
 
-filename_out = "merged_subtypes_" + res_savename
-filename_out_geo = "merged_subtypes_" + res_savename_geo
+filename_out = res_savename
+filename_out_geo = res_savename_geo
 
 #set up logger
 logger_name = "subtypes_merger"
